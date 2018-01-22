6024e143438ca9b92a4adc811878e8b120fa7cf7
Saman Khan <samank3@hotmail.com>
2018-01-22 11:53:53 -0500

    new method

--- src/Ding.java
+++ src/Ding.java
@@ -4,6 +4,12 @@ public class Ding {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//eh
+		
+		public static void doesNothing()
+		{
+			System.out.println("I do nothing");
+		}
+		
 	}
 
 }
