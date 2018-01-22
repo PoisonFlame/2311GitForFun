f5198d6da134a6d69bcc88bd2e394cbbf47c6f19
Sharmazz <robbysharma@me.com>
2018-01-22 12:00:31 -0500

    hi

--- src/Ding.java
+++ src/Ding.java
@@ -1,14 +1,17 @@
 
 public class Ding {
 
+	
+	public static void doesNothing()
+	{
+		System.out.println("I do nothing");
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//eh
-		
-		public static void doesNothing()
-		{
-			System.out.println("I do nothing");
-		}
+		doesNothing();
 		
 	}
 
