dbfc8bc94e7f65b379dfb36e66e9113f878de7c1
Sharmazz <robbysharma@me.com>
2018-01-22 13:00:49 -0500

    Merge remote-tracking branch 'origin/branc2'
    Conflicts:
    	src/Ding.java

--- src/Ding.java
+++ src/Ding.java
@@ -4,15 +4,13 @@ public class Ding {
 	
 	public static void doesNothing()
 	{
-		System.out.println("I do nothing");
+		System.out.println("I do nothing!");
 	}
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//eh
-		System.out.println("hi");
-		
 	}
 
 	// hello
